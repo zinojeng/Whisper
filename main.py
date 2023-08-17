50b7c3bd57769ae7b5fdb84bf4eaca2a35ff8d21
zinojeng <zino@livemail.tw>
2023-08-18 06:07:05 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import openai
+import streamlit as st
+import os
+
+st.title("Audio to Text")
+
+# Get the open AI API Key
+api_key = st.text_input(label="OpenAI API Key", placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key", help="You can get your API key from https://platform.openai.com/account/api-keys/")
+os.environ["OPENAI_API_KEY"] = api_key
+
+# File uploader
+audio = st.file_uploader(label="Upload audio file", type=['wav', 'mp3', 'm4a'])
+
+if audio is not None:
+   response = openai.Audio.transcribe("whisper-1", audio, verbose=True, api_key=api_key)
+   st.write(response["text"])
